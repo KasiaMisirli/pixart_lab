f2e729141ce3558c1a7478e5bc5f8525d046338a
kasia-misirli <kasia.misirli@gmail.com>
2018-04-30 16:21:31 +1000

    adding enter submit

--- script.js
+++ script.js
@@ -2,3 +2,12 @@ document.getElementById("mybtn").addEventListener("click", function(){
     var input = document.getElementById("inp").value;
     document.getElementById("box").style.backgroundColor = input;
 });
+
+var input = document.getElementById("inp");
+
+input.addEventListener("keyup", function(event) {
+  event.preventDefault();
+  if (event.keyCode === 13) {
+    document.getElementById("mybtn").click();
+  }
+});
