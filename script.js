aa9c39e84f00347888be9c4e98d64b1e3e00b5ca
kasia-misirli <kasia.misirli@gmail.com>
2018-04-30 16:11:10 +1000

    adding colour brush

---
+++ script.js
@@ -0,0 +1,4 @@
+document.getElementById("mybtn").addEventListener("click", function(){
+    var input = document.getElementById("inp").value;
+    document.getElementById("box").style.backgroundColor = input;
+});
